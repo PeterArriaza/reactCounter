94070a042e8ea5cf0899d8bcaba35edfd5584699
PeterArriaza <peter.arriaza@gmail.com>
2020-03-27 16:21:26 -0400

    Updates

---
+++ precache-manifest.f3f1f2ea5606b06c87809e6be0f255a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e833000eb0b4008b575cb9a3ec74ce8",
+    "url": "/reactCounter/index.html"
+  },
+  {
+    "revision": "6a8994247928259e7245",
+    "url": "/reactCounter/static/css/main.af58a7e5.chunk.css"
+  },
+  {
+    "revision": "fe4255ec52e206507221",
+    "url": "/reactCounter/static/js/2.628252c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactCounter/static/js/2.628252c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a8994247928259e7245",
+    "url": "/reactCounter/static/js/main.a33e4120.chunk.js"
+  },
+  {
+    "revision": "812e0fc534ba44caa12a",
+    "url": "/reactCounter/static/js/runtime-main.e471cb93.js"
+  }
+]);
